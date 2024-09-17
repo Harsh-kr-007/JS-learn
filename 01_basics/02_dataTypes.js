3e73564a56dedf18cfd10a767f2381a71e5dcefb
Harsh-kr-007 <harsha16179@gmail.com>
2024-09-17 02:57:11 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict";  // treat all JS code as a newer version
+// alert(3 + 3) //we are using node js not browser
+
+let name = "Harsh"
+let age = 18
+let isLoggedin = false
+let state;  //undefined
+
+
+
+// number => 2 to power 53
+//bigint
+//string => " "
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+//object =>
+
+console.log(typeof "Harsh");    //string
+console.log(typeof null);       //object
+console.log(typeof undefined);  //undefined
+
+
