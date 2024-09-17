6de26ec8dd08d77f21c65d8574b01fd56d30e8c2
Harsh-kr-007 <harsha16179@gmail.com>
2024-09-17 02:59:55 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const consumerId = 4321
+var consumerMail = "hk@google.com "
+let accPassword = "12345"
+//consumerId = 1234  not allowed
+accCity = "Gaya"
+let accState
+
+console.table([consumerId,consumerMail,accPassword,accCity,accState])
+
+/* prefer not to use var because of issues in block scope and
+functional scope*/
