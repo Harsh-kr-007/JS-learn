051d297c5c0dfa24f1a06af369462ff332ed8710
Harsh-kr-007 <harsha16179@gmail.com>
2024-09-23 10:20:05 +0000

    Conversion of Datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -15,12 +15,53 @@ let valueInNumber = Number(score);
 let isLoggedin = 0;
 
 let booleanIsLoggedIn = Boolean(isLoggedin)
-console.log(booleanIsLoggedIn);  //1 => true ; 0 => false
+//console.log(booleanIsLoggedIn);  //1 => true ; 0 => false
                                  // "" => false ; "Harsh" => true
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof(stringNumber));
+//console.log(stringNumber);
+//console.log(typeof(stringNumber));
+
+
+
+//  ************************************ Operations ************************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/2);
+//console.log(2%3);
+
+let str1 = "hello";
+let str2 = " harsh";
+let str3 = str1 + str2;
+//console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 +2 + "2");
+//console.log(+true);
+//console.log(+"");
+//console.log();
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);   
+
+
+
+
+
+
+
+
+
 
                                 
 
